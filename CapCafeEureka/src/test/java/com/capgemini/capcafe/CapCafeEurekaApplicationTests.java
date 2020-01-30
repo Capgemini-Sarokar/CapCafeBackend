baf78a992624fec4dd4a4dbfca804715e9fcc409
Kunal Roychoudhury <lanuks97@gmail.com>
2020-01-30 22:35:01 +0530

    Initialized Projects

---
+++ CapCafeEureka/src/test/java/com/capgemini/capcafe/CapCafeEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.capcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapCafeEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ CapCafeLoginRegistrationService/src/test/java/com/capgemini/capcafe/loginregistration/CapCafeLoginRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.capcafe.loginregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapCafeLoginRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
