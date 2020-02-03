9b5d79e1bd633f2d6f26d58900c4c582ff72085c
Kunal Roychoudhury <lanuks97@gmail.com>
2020-02-03 12:04:35 +0530

    added Cafe Details Service project

---
+++ CapCafeCafeDetails/src/main/java/com/capgemini/capcafe/cafedetails/CapCafeCafeDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.capcafe.cafedetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapCafeCafeDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapCafeCafeDetailsApplication.class, args);
+	}
+
+}
---
+++ CapCafeCafeDetails/src/test/java/com/capgemini/capcafe/cafedetails/CapCafeCafeDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.capcafe.cafedetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapCafeCafeDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
