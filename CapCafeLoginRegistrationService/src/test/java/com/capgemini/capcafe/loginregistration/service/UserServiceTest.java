4a18728315fc5af205dc1ae9948e0ed47fb7c38d
Kunal Roychoudhury <lanuks97@gmail.com>
2020-02-06 01:14:21 +0530

    Added Test Cases for Login

--- CapCafeLoginRegistrationService/src/test/java/com/capgemini/capcafe/loginregistration/service/UserServiceTest.java
+++ CapCafeLoginRegistrationService/src/test/java/com/capgemini/capcafe/loginregistration/service/UserServiceTest.java
@@ -1,5 +1,51 @@
 package com.capgemini.capcafe.loginregistration.service;
 
-public class UserServiceTest {
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+import java.util.Calendar;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.capgemini.capcafe.loginregistration.entity.UserEntity;
+import com.capgemini.capcafe.loginregistration.exception.UserException;
+import com.capgemini.capcafe.loginregistration.persistence.UserPersistence;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+	
+	@InjectMocks
+    UserService userService = new UserServiceImpl();
+    
+	@Mock
+	BCryptPasswordEncoder passwordEncoder;
+	
+    @Mock
+    UserPersistence userDao;
+    
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @Test
+    public void signUpTest () throws UserException {
+    	Calendar dob = Calendar.getInstance();
+		dob.set(1997, 1, 1);
+        UserEntity user = new UserEntity ("46001645", "kunal.roychoudhury@capgemini.com",
+				"password", "Kunal Roychoudhury", "6234567891", dob, "Admin");
+        
+        Mockito.when(userDao.save(Mockito.any(UserEntity.class))).thenReturn(user);
+        this.userService.signUp(user);
+         
+        verify(userDao, times(1)).save(user);
+    }
 }
