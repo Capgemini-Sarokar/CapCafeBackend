4f1d19d711d43f9410ab0c3ab36e37fc4edf6859
Rishav Dev <55138547+DevRishav@users.noreply.github.com>
2020-02-06 01:23:19 +0530

    Test packages for cafe

--- CapCafeCafeDetails/src/main/java/com/capgemini/capcafe/cafedetails/controller/CafeController.java
+++ CapCafeCafeDetails/src/main/java/com/capgemini/capcafe/cafedetails/controller/CafeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capgemini.capcafe.cafedetails.entity.Cafe;
+import com.capgemini.capcafe.cafedetails.exception.CafeException;
 import com.capgemini.capcafe.cafedetails.service.CafeService;
 
 @CrossOrigin("http://localhost:4200")
@@ -27,22 +28,23 @@ public class CafeController {
 	}
 
 	@PostMapping(path = "/getCafeById")
-	public Cafe getCafeById(@RequestBody String cafeID) {
-		return null;
+	public Cafe getCafeById(@RequestBody String cafeID) throws CafeException {
+		return this.cafeService.fetchCafeById(cafeID);
 	}
 
 	@PostMapping(path = "/removeCafeById")
-	public Cafe removeCafe(@RequestBody String cafeID) {
-		return null;
+	public boolean removeCafe(@RequestBody String cafeID) throws CafeException {
+		return this.cafeService.removeCafe(cafeID);
+	
 	}
 
 	@GetMapping(path = "/getAllCafe")
 	public List<Cafe> getAllCafe() {
-		return null;
+		return this.cafeService.fetchAllCafe();
 	}
 	
 	@PostMapping(path = "/updateCafe")
 	public Cafe updateCafe (@RequestBody Cafe cafe) {
-		return null;
+		return this.cafeService.updateCafe(cafe);
 	}
 }
---
+++ CapCafeCafeDetails/src/test/java/com/capgemini/capcafe/cafedetails/cafeController/cafeControllerTest.java
@@ -0,0 +1,5 @@
+package com.capgemini.capcafe.cafedetails.cafeController;
+
+public class cafeControllerTest {
+
+}
