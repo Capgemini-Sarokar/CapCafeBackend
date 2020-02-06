7c527fadbf8cbf678133b6201b65e3ef47f76f8b
Anwesha98 <55138807+Anwesha98@users.noreply.github.com>
2020-02-06 07:22:16 +0530

    Pom changes in helpdesk

--- CapCafeHelpdesk/src/main/java/com/capgemini/capcafe/helpdesk/service/HelpdeskService.java
+++ CapCafeHelpdesk/src/main/java/com/capgemini/capcafe/helpdesk/service/HelpdeskService.java
@@ -1,6 +1,7 @@
 package com.capgemini.capcafe.helpdesk.service;
 import java.util.List;
 
+import com.capgemini.capcafe.helpdesk.controller.Ticket;
 import com.capgemini.capcafe.helpdesk.exception.HelpdeskException;
 
 public interface HelpdeskService {
@@ -9,6 +10,8 @@ List<Orders> getAllOrders();
 	Orders addNewOrder(Orders order);
 	
 	Orders getById(int orderId);
+
+	Ticket raiseTicket(int id, String msg);
 
 
 }
